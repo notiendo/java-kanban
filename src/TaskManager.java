157fc5f6064ee635948971d6320128dea3979c8c
User Alexander <sashaivanov174@gmail.com>
2025-08-19 03:02:18 +0500

    Убрать ошибку со стилем в TaskManager

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,27 +1,45 @@
 import java.util.List;
 
 public interface TaskManager {
+
     List<Task> getAllTasks();
+
     void deleteAllTasks();
+
     Task getTaskById(int id);
+
     Task createTask(Task task);
+
     void updateTask(Task task);
+
     void deleteTaskById(int id);
 
     List<Epic> getAllEpics();
+
     void deleteAllEpics();
-    Epic getEpicById(int id);  // Оставляем только getEpicById
+
+    Epic getEpicById(int id);
+
     Epic createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void deleteEpicById(int id);
 
     List<Subtask> getAllSubtasks();
+
     void deleteAllSubtasks();
-    Subtask getSubtaskById(int id);  // Оставляем только getSubtaskById
+
+    Subtask getSubtaskById(int id);
+
     Subtask createSubtask(Subtask subtask);
+
     void updateSubtask(Subtask subtask);
+
     void deleteSubtaskById(int id);
 
+
     List<Task> getHistory();
+
     List<Subtask> getSubtasksByEpicId(int epicId);
 }
